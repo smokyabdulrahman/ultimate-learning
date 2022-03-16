e23a1f695f186587bc92509d8bd7a4fcb0d080d1
ABDULRAHMAN ALRAHMA <aalrahma@red.sa>
2022-03-16 19:22:03 +0300

    init spring project

---
+++ backend/src/main/java/me/alrahma/ultimatelearning/UltimateLearningApplication.java
@@ -0,0 +1,13 @@
+package me.alrahma.ultimatelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UltimateLearningApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/me/alrahma/ultimatelearning/UltimateLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alrahma.ultimatelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
